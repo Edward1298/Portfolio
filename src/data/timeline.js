bc8858f31493724b05f6138bca681e0e90a6a288
Edward1298 <Edward_1298@hotmail.com>
2026-07-21 16:14:00 -0600

    feat(phases 3-4): redesign landing hero and about timeline section

---
+++ src/data/timeline.js
@@ -0,0 +1,7 @@
+export const entries = [
+  { date: 'May 2020', role: 'Systems Engineering', place: 'Fidelitas University' },
+  { date: 'January 2022', role: 'Web Developer', place: 'Instituto de Estudio y Desarrollo Humano' },
+  { date: 'June 2024', role: 'Software Development Intern', place: 'Align Technology' },
+  { date: 'December 2024', role: 'Software Developer / DevOps Engineer', place: 'One Way Technologies' },
+  { date: 'Present', role: null, place: null },
+]
